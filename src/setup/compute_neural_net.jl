9de4c1b2719ba6ace2e85c6e406e07246ff6e9e5
Aaron Graybill <aarongraybill12@gmail.com>
2022-06-07 14:29:45 -0400

    Recursive neural net computation

---
+++ src/setup/compute_neural_net.jl
@@ -0,0 +1,32 @@
+using NeuralTests
+
+
+"""
+Given a neural 
+
+"""
+function compute_neural_net(net::Network,lookup_table::Dict{Node,Float64},output_nodes::Set{Node})
+    @assert keys(lookup_table) ⊆ net.nodes
+    @assert output_nodes ⊆ net.nodes
+    output_vals = Dict{Node,Float64}()
+    [o.opt_text for o in output_nodes]
+    for node in output_nodes
+        run_sum = 0.0
+        origins = setdiff([e.origin for e ∈ node.edges],[node]) #exclude nodes where you are the input
+
+        for o_node ∈ keys(lookup_table) ∩ origins # the values we do have
+            temp_val = get(lookup_table,o_node,"Value Not found")
+            temp_weight = get_weight(net,o_node,node)
+            run_sum = run_sum+temp_val*temp_weight
+        end
+        for o_node ∈ setdiff(origins,keys(lookup_table)) # the values we don't have
+            temp_weight = get_weight(net,o_node,node)
+            temp_val_dict = compute_neural_net(net,lookup_table,Set([o_node]))
+            temp_val = get(temp_val_dict,o_node,"uh-oh")
+            run_sum = run_sum+temp_weight*temp_val
+        end
+        merge!(lookup_table,Dict(node => run_sum))
+        merge!(output_vals,Dict(node => run_sum))
+    end
+    return output_vals
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,28 @@
-using NeuralTests
+import NeuralTests as n
 using Test
 
+Node1 = n.Node("Node1")
+Node2 = n.Node("Node2")
+Edge1 = n.Edge(Node1,Node2)
+net_auto = n.Network(Set([Node1,Node2]),Set([Edge1]),Dict(Edge1=>5.0))
+n.connect_network!(net_auto)
+
+# Manually add edges to nodes to check if auto works
+Node1.edges = Set([Edge1])
+Node2.edges = Set([Edge1])
+net = n.Network(Set([Node1,Node2]),Set([Edge1]),Dict(Edge1=> 5.0))
+
+Node3 = n.Node()
+Edge2 = n.Edge(Node1,Node3)
+push!(Node1.edges,Edge2)
+push!(Node3.edges,Edge2)
+bad_net = n.Network(Set([Node1,Node2]),Set([Edge1,Edge2]),Dict(Edge1=>5.0,Edge2=>6.0))
+
+new_net = deepcopy(net)
+n.add_node!(new_net,node_is_origin_for=Dict(n.get_nodes(new_net,"Node1")=>5.0),opt_text="lil_guy")
+
 @testset "NeuralTests.jl" begin
-    # Write your tests here.
+    @test get(new_net.weights,n.get_edges(new_net,"lil_guy","Node1"),"Not Found")==5
+    @test n.network_check(bad_net)=="Edges out of bounds"
+    @test [x for x in net_auto.weights]==[x for x in net.weights]
 end
